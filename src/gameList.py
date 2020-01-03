12929aaebc63523fc3eccf5534d1010fb2860545
Sakshat Shinde <shindesakshat@gmail.com>
2020-01-03 13:38:12 +0530

    gameAdd() now only accepts Store names
    Possible gameStore values = STEAM, UPLAY, PIRATED, EGS, ORIGIN, GOG

--- src/gameList.py
+++ src/gameList.py
@@ -1,5 +1,15 @@
+import enum
 class GameList:
     def __init__(self, name, gameId, gameStore):
         self.name = name 
         self.gameId = gameId
         self.gameStore = gameStore
+
+class GameStore(enum.Enum):
+    STEAM = "STEAM"
+    EGS = "EGS"
+    UPLAY = "UPLAY"
+    ORIGIN = "ORIGIN"
+    GOG = "GOG"
+    PIRATED = "PIRATED"
+    
--- src/gameOps.py
+++ src/gameOps.py
@@ -1,14 +1,24 @@
-from gameList import GameList
+from gameList import GameList, GameStore
 import os, fnmatch, re
 
 #Master game list for all the games regardless of the launcher
 GAME_LIST_MASTER = {}
 STEAM_ID = []
 
-#Add steam games to the main list
-def gameAddSteam(name, gameId, gameStore):
-    game = GameList(name, gameId, gameStore)
-    GAME_LIST_MASTER[game.name] = [game.gameId, gameStore]
+'''
+Add steam games to the main list
+Possible gameStore values = STEAM, UPLAY, PIRATED, EGS, ORIGIN, GOG
+GameStore is an enum and the class has all the specified values : EGS, ORGIN etc
+gameStore: GameStore makes sure that the gameStore is of type GameStore
+gameStore.value is the value of the given variables in the GameStore class 
+>so for example if we do gameAdd('doki doki', 123234, GameStore.EGS)
+>we get the value of the EGS variable from the GameStore into -> the GAME_LIST_MASTER
+'''
+
+def gameAdd(name, gameId, gameStore: GameStore): 
+    game = GameList(name, gameId, gameStore.value)
+    GAME_LIST_MASTER[game.name] = [game.gameId, gameStore.value]
+
 
 '''
 Get the steamapps/ folder path
@@ -22,15 +32,11 @@ def getSteamIDs(filePath):
     pattern = "*.acf"
     for entry in listOfFiles:
         if fnmatch.fnmatch(entry, pattern):
-            extractedSteamID = re.findall(r'\d+', str(entry)) 
+            extractedSteamID = re.findall(r'\d+', str(entry)) # '\d' finds any number (a digit)
             STEAM_ID.append(extractedSteamID[0])
             
-
-
-
-
 #getSteamIDs(r"D:\SteamLibrary\steamapps")
 #print(STEAM_ID)
-#gameAddSteam("Doki Doki", 698780, "Steam")
+#gameAdd("Doki Doki", 698780, GameStore.EGS)
 #print(GAME_LIST_MASTER)
 #D:\SteamLibrary\steamapps
