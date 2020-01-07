4a2fa44333e097315e34e4f8fa34e74f7f5b938f
Sakshat Shinde <shindesakshat@gmail.com>
2020-01-07 10:14:13 +0530

    Create usrOps.py
    This part will directly interact with the user

---
+++ src/usrOps.py
@@ -0,0 +1,15 @@
+from gameOps import storeDirectory, GAME_DIRS, GAME_LIST_MASTER
+
+# User input for ORIGN/UPLAY/PIRATED directories
+gameStore, storeDir = input('Game Store name: '), input('Game store Dir: ')
+
+#For Steam
+if(gameStore == "STEAM"):
+    storeDir = storeDir + '\\steamapps\\'
+    storeDirectory(gameStore, storeDir)
+
+#For Others
+else:
+    storeDir = storeDir + '\\'
+    storeDirectory(gameStore, storeDir)
+
