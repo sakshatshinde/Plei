b3af8c90f5651cea7cb99c1064a7f565d392da4c
Sakshat Shinde <shindesakshat@gmail.com>
2020-01-11 23:27:08 +0530

    Added Title for the window

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -33,12 +33,27 @@ def onLeaveClose(e):
         
 
 root = Tk()
+
 # removes title bar 
 root.overrideredirect(True)
+
 root.configure(background = BG_VAL, height = 800, width = 1600)
 root.resizable(FALSE, FALSE)
+
 # sets the window in center of the screen
 root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(root.winfo_id()))
+
+#Title 
+title = Label(  root, 
+                text = 'Plei - A game launcher with no bloat',
+                fg = '#039be5',
+                background = BG_VAL,
+                font=('Helvetica', 11, 'bold'),
+                relief = FLAT
+)
+title.place(x = root.winfo_width()/2, y = 20 , anchor= 'center')
+#title.place(x = 5, y = 5)
+
 
 # Minimize button
 minIcon = PhotoImage(file = r'resources/minus.png')
