e61cd9752a9f85b6fda7d00828b73d23a5d9b1bb
Sakshat Shinde <shindesakshat@gmail.com>
2020-01-20 20:32:56 +0530

    Fixed weird Windows file naming bugs

--- src/gui/imgServe.py
+++ src/gui/imgServe.py
@@ -1,5 +1,16 @@
 import urllib.request
+import re 
 
 def retrieveImg(gameName, imgURL):
+    gameName = legalFileName(gameName)     # Windows doesn't like weird stuff in filenames
     location = 'data\\img\\'
     urllib.request.urlretrieve(imgURL, (location + gameName + ".png"))
+
+# Making sure windows doesn't cry if : or similar characters appear in the game name
+def legalFileName(inString):
+    checkList = ['<', '>', ':', '"', '/', '\\', '|', '?', '?', '*', '.', ',']
+    for i in checkList:
+        if i in inString:
+            inString = inString.replace(i, '')
+    return inString
+
