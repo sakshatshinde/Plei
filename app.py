930832d8ea492bf0385043332db3cf6c215ea0e0
Sakshat Shinde <shindesakshat@gmail.com>
2020-02-11 17:02:57 +0530

    Restructuring the project

---
+++ app.py
@@ -0,0 +1,8 @@
+# from usrOps import *
+from src.gui.gui import plei
+
+if __name__ == "__main__":
+    # selection()
+    plei()
+
+
--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,201 +1,203 @@
-from tkinter import *
-from tkinter import ttk
-import tkinter.font as fontyPoo # Sorry for the weird name :p
-import winsound, pickle, fetchData, asyncio
+def plei():
+    import tkinter as tk
+    from tkinter import ttk
+    import tkinter.font as fontyPoo # Sorry for the weird name :p
+    import winsound, pickle
+    # import fetchData
 
-# Dirty fix --------
-import sys
-sys.path.append('D:\\dev\\Plei\\src')
-from gameOps import readData, sync, launchGame
-# -----------
+    # Dirty fix --------
+    import sys
+    sys.path.append('D:\\dev\\Plei\\src')
+    from gameOps import readData, sync, launchGame
+    # -----------
 
-BG_VAL = "#212121"
-FG_VAL = "#D2BF55"
+    BG_VAL = "#212121"
+    FG_VAL = "#D2BF55"
 
-def close(): #Close the window
-    root.destroy()
+    def close(): #Close the window
+        root.destroy()
 
-def minWin():   #minimze the window
-    root.withdraw()
-    root.overrideredirect(False)
-    root.iconify()
+    def minWin():   #minimze the window
+        root.withdraw()
+        root.overrideredirect(False)
+        root.iconify()
 
-def check_map(event): # apply override on deiconify.
-    if str(event) == "<Map event>":
-        root.overrideredirect(True)
-    else:
-        pass
+    def check_map(event): # apply override on deiconify.
+        if str(event) == "<Map event>":
+            root.overrideredirect(True)
+        else:
+            pass
 
-# def rating():
-#     ratingList = []
-#     allGames = GAMES
-#     for game in allGames:
-#         ratingList.append(fetchData.gameRating(game))
-#     final = dict(zip(allGames, ratingList))
-#     print(final)
+    # def rating():
+    #     ratingList = []
+    #     allGames = GAMES
+    #     for game in allGames:
+    #         ratingList.append(fetchData.gameRating(game))
+    #     final = dict(zip(allGames, ratingList))
+    #     print(final)
 
-# rating()
+    # rating()
 
-# Hover functions
-def onEnterMin(e):
-    minBtn['background'] = '#424242'
+    # Hover functions
+    def onEnterMin(e):
+        minBtn['background'] = '#424242'
 
-def onLeaveMin(e):
-    minBtn['background'] = BG_VAL
+    def onLeaveMin(e):
+        minBtn['background'] = BG_VAL
 
-def onEnterClose(e):
-    closeBtn['background'] = '#424242'
+    def onEnterClose(e):
+        closeBtn['background'] = '#424242'
 
-def onLeaveClose(e):
-    closeBtn['background'] = BG_VAL
-
-
-
-root = Tk()
-
-# removes title bar 
-root.overrideredirect(True)
-
-# Window config
-root.configure(background = BG_VAL, height = 800, width = 1600)
-root.resizable(FALSE, FALSE)
-
-# sets the window in center of the screen
-root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(root.winfo_id()))
-
-# Title 
-titleLine = Label(  root, 
-                text = 'Plei - A game launcher with no bloat',
-                fg = '#039be5',
-                background = BG_VAL,
-                font=('Helvetica', 11, 'bold'),
-                relief = FLAT
-)
-titleLine.place(x = root.winfo_width()/2, y = 20 , anchor= 'center')
+    def onLeaveClose(e):
+        closeBtn['background'] = BG_VAL
 
 
-# Game list
-sync()  # Update List STATE
-GAMES = readData() # Read from plei data file
-customFont = fontyPoo.Font(size = 12)
-gameList = Listbox(root, 
-                    bg = BG_VAL, 
-                    bd = 20,
-                    fg = '#ffab40',
-                    relief = FLAT, 
-                    highlightthickness = 5,
-                    selectmode = SINGLE,
-                    height = 35,
-                    highlightcolor = '#f2209e',
-                    selectbackground = BG_VAL,
-                    selectforeground = '#039be5',
-                    width = 0,
-                    activestyle = NONE,
-                    font = customFont
-                    )
-                    
-gameList.yview()    #vertical scroll
-gameList.insert(0, *GAMES)
-gameList.place(x = root.winfo_width()/55, y = 40, anchor = NW )
 
-# A class to save object state
-class Selection:
-    currentSelection = ''
+    root = tk.Tk()
 
-# Selecting the game from the list 
-def curSelect(evt):
-    # Save the selection state inside the class variable
-    Selection.currentSelection = gameList.get(ANCHOR)
-gameList.bind('<<ListboxSelect>>',curSelect)
+    # removes title bar 
+    root.overrideredirect(True)
 
-# Launching the selected game
-def launch():
-    launchGame(Selection.currentSelection)
+    # Window config
+    root.configure(background = BG_VAL, height = 800, width = 1600)
+    root.resizable(tk.FALSE, tk.FALSE)
 
-# Launch button label
-launchLabel = Label(  root, 
-                text = 'Plei ',
-                fg = '#039be5',
-                background = BG_VAL,
-                font = ('Helvetica', 20, 'bold'),
-                relief = FLAT
-)
-launchLabel.place(x = root.winfo_width()/2, y = root.winfo_height()/2.5, anchor= CENTER)
+    # sets the window in center of the screen
+    root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(root.winfo_id()))
 
-# Launch Button
-launchIcon = PhotoImage(file = 'resources\\tic-tac-toe.png')
-launchIcon = launchIcon.subsample(5,5)
-launchBtn = Button(
-    root, 
-    image = launchIcon,  
-    background=BG_VAL, 
-    command = launch,
-    activebackground = BG_VAL, 
-    borderwidth = 0
-) 
-launchBtn.place(x = root.winfo_width()/2, y = root.winfo_height()/2, anchor = CENTER)
-# x = 1200
-
-# Header
-title = Label(  root, 
-                text = 'Games ',
-                fg = '#039be5',
-                background = BG_VAL,
-                font = ('Helvetica', 20, 'bold'),
-                relief = FLAT
-)
-title.place(x = root.winfo_width()/72, y = 60 , anchor= SW)
-
-# # Image
-# img = PhotoImage(file = 'data\\img\\Apex legends.png') # Test image
-# label = Label(image = img)
-# label.place(x = root.winfo_width()/2, y = root.winfo_height()/2, anchor = CENTER )
+    # Title 
+    titleLine = tk.Label(  root, 
+                    text = 'Plei - A game launcher with no bloat',
+                    fg = '#039be5',
+                    background = BG_VAL,
+                    font=('Helvetica', 11, 'bold'),
+                    relief = tk.FLAT
+    )
+    titleLine.place(x = root.winfo_width()/2, y = 20 , anchor= 'center')
 
 
-# Footer
-footer = Label(  root, 
-                text = 'by ScreX ',
-                fg = '#039be5',
-                background = BG_VAL,
-                font = ('Helvetica', 14, 'bold'),
-                relief = FLAT
-)
-footer.place(x = root.winfo_width()/65, y = 765 , anchor= SW)
+    # Game list
+    sync()  # Update List STATE
+    GAMES = readData() # Read from plei data file
+    customFont = fontyPoo.Font(size = 12)
+    gameList = tk.Listbox(root, 
+                        bg = BG_VAL, 
+                        bd = 20,
+                        fg = '#ffab40',
+                        relief = tk.FLAT, 
+                        highlightthickness = 5,
+                        selectmode = tk.SINGLE,
+                        height = 35,
+                        highlightcolor = '#f2209e',
+                        selectbackground = BG_VAL,
+                        selectforeground = '#039be5',
+                        width = 0,
+                        activestyle = tk.NONE,
+                        font = customFont
+                        )
+                        
+    gameList.yview()    #vertical scroll
+    gameList.insert(0, *GAMES)
+    gameList.place(x = root.winfo_width()/55, y = 40, anchor = tk.NW )
 
-# Minimize button
-minIcon = PhotoImage(file = 'resources\\minus.png')
-minIcon = minIcon.subsample(20,20)
-minBtn = Button(
-    root, 
-    image = minIcon,  
-    background=BG_VAL, 
-    command = minWin,
-    activebackground = BG_VAL, 
-    borderwidth = 0
-) 
-minBtn.place(relx = 1, x = -38, y = 5, anchor = NE)
+    # A class to save object state
+    class Selection:
+        currentSelection = ''
 
-# Close button
-closeIcon = PhotoImage(file = 'resources\\close.png')
-#Resizing the image for the button to fit
-closeIcon = closeIcon.subsample(20,20)
-closeBtn = Button(
-    root, 
-    image = closeIcon , 
-    command=close, 
-    background=BG_VAL, 
-    activebackground = BG_VAL,
-    borderwidth = 0
-) 
-closeBtn.place(relx = 1, x =-5, y = 5, anchor = NE)
+    # Selecting the game from the list 
+    def curSelect(evt):
+        # Save the selection state inside the class variable
+        Selection.currentSelection = gameList.get(tk.ANCHOR)
+    gameList.bind('<<ListboxSelect>>',curSelect)
 
-# Hover for buttons
-minBtn.bind("<Enter>", onEnterMin)
-minBtn.bind("<Leave>", onLeaveMin)
-closeBtn.bind("<Enter>", onEnterClose)
-closeBtn.bind("<Leave>", onLeaveClose)
+    # Launching the selected game
+    def launch():
+        launchGame(Selection.currentSelection)
 
-# Added bindings to pass windows status to function : MAXIMIZE AND MINIMIZE
-root.bind('<Map>', check_map) 
-root.bind('<Unmap>', check_map)
-root.mainloop()
+    # Launch button label
+    launchLabel = tk.Label(  root, 
+                    text = 'Plei ',
+                    fg = '#039be5',
+                    background = BG_VAL,
+                    font = ('Helvetica', 20, 'bold'),
+                    relief = tk.FLAT
+    )
+    launchLabel.place(x = root.winfo_width()/2, y = root.winfo_height()/2.5, anchor= tk.CENTER)
+
+    # Launch Button
+    launchIcon = tk.PhotoImage(file = 'resources\\tic-tac-toe.png')
+    launchIcon = launchIcon.subsample(5,5)
+    launchBtn = tk.Button(
+        root, 
+        image = launchIcon,  
+        background=BG_VAL, 
+        command = launch,
+        activebackground = BG_VAL, 
+        borderwidth = 0
+    ) 
+    launchBtn.place(x = root.winfo_width()/2, y = root.winfo_height()/2, anchor = tk.CENTER)
+    # x = 1200
+
+    # Header
+    title = tk.Label(  root, 
+                    text = 'Games ',
+                    fg = '#039be5',
+                    background = BG_VAL,
+                    font = ('Helvetica', 20, 'bold'),
+                    relief = tk.FLAT
+    )
+    title.place(x = root.winfo_width()/72, y = 60 , anchor= tk.SW)
+
+    # # Image
+    # img = PhotoImage(file = 'data\\img\\Apex legends.png') # Test image
+    # label = Label(image = img)
+    # label.place(x = root.winfo_width()/2, y = root.winfo_height()/2, anchor = CENTER )
+
+
+    # Footer
+    footer = tk.Label(  root, 
+                    text = 'by ScreX ',
+                    fg = '#039be5',
+                    background = BG_VAL,
+                    font = ('Helvetica', 14, 'bold'),
+                    relief = tk.FLAT
+    )
+    footer.place(x = root.winfo_width()/65, y = 765 , anchor = tk.SW)
+
+    # Minimize button
+    minIcon = tk.PhotoImage(file = 'resources\\minus.png')
+    minIcon = minIcon.subsample(20,20)
+    minBtn = tk.Button(
+        root, 
+        image = minIcon,  
+        background=BG_VAL, 
+        command = minWin,
+        activebackground = BG_VAL, 
+        borderwidth = 0
+    ) 
+    minBtn.place(relx = 1, x = -38, y = 5, anchor = tk.NE)
+
+    # Close button
+    closeIcon = tk.PhotoImage(file = 'resources\\close.png')
+    #Resizing the image for the button to fit
+    closeIcon = closeIcon.subsample(20,20)
+    closeBtn = tk.Button(
+        root, 
+        image = closeIcon , 
+        command=close, 
+        background=BG_VAL, 
+        activebackground = BG_VAL,
+        borderwidth = 0
+    ) 
+    closeBtn.place(relx = 1, x =-5, y = 5, anchor = tk.NE)
+
+    # Hover for buttons
+    minBtn.bind("<Enter>", onEnterMin)
+    minBtn.bind("<Leave>", onLeaveMin)
+    closeBtn.bind("<Enter>", onEnterClose)
+    closeBtn.bind("<Leave>", onLeaveClose)
+
+    # Added bindings to pass windows status to function : MAXIMIZE AND MINIMIZE
+    root.bind('<Map>', check_map) 
+    root.bind('<Unmap>', check_map)
+    root.mainloop()
