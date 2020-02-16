ad2235cfc971f2c3466b39ff14b4d9324872c0d8
Sakshat Shinde <shindesakshat@gmail.com>
2020-02-16 19:05:02 +0530

    Revert "test_build"
    This reverts commit 0e37a49ac13b66ace45df2819f15e5dfdfa91244.

--- src/gameOps.py
+++ src/gameOps.py
@@ -1,10 +1,10 @@
-from src.gameList import Game, GameStore
+from gameList import Game, GameStore
 from steamfiles import acf 
 from collections import OrderedDict
 from nested_lookup import nested_lookup
 import os, fnmatch , pickle, re, json
 import winreg #for Uplay
-from src.usrOps import  readDirList
+from usrOps import  readDirList
 
 # Master game list for all the games regardless of the launcher
 GAME_LIST_MASTER = {}
