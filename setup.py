d5ff6cfdd0b1e4cd8be7e7613d1fbd2ecf7319a5
Sakshat Shinde <shindesakshat@gmail.com>
2020-02-19 12:16:12 +0530

    Plei 0.1 setup

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from cx_Freeze import setup, Executable
 import sys
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-
     "packages": ["os","steamfiles","nested_lookup","json","src.gameOps","src.usrOps"], 
-    "includes": ["tkinter","os","steamfiles","nested_lookup","json","src.gameOps","src.usrOps"]}
+    "includes": ["tkinter","os","steamfiles","nested_lookup","json","src.gameOps","src.usrOps"]
+}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -16,4 +16,10 @@ setup(  name = "Plei",
         version = "0.1",
         description = "A bloat free game launcher",
         options = {"build_exe": build_exe_options},
-        executables = [Executable("app.py", base=base)])
+        executables = [Executable(
+            "app.py", 
+            base=base,
+            icon="Plei.ico"
+            )
+        ]
+    )
